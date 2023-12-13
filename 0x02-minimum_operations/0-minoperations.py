0fc7f94d5938bfc9e64e2880152dfe08298c83ac
Obioma29 <obiomachinwuko@yahoo.com>
2023-12-13 12:49:34 +0000

    Minimum Operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" A Script that shows the  minimum operations
+    needed in a CopyAll - Paste operation
+"""
+
+
+def minOperations(n):
+    """Method for compute the minimum number
+    of operations for task Copy All and Paste"""
+
+    z = 'H'
+    i  = 'H'
+    op = 0
+    while (len(i) < n):
+        if n % len(i) == 0:
+            op += 2
+            z = i
+            i += i
+        else:
+            op += 1
+            i += z
+    if len(i) != n:
+        return 0
+    return op
